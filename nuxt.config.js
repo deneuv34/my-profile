4e29c396893831c6f7e9637b36cbf578ff3aff40
Rangga Adhitya Prawira <deneuv3.4@gmail.com>
2018-09-15 15:13:43 +0700

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Based on Nuxtjs' }
+      { hid: 'description', name: 'description', content: 'Rangga Adhitya Prawira' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
